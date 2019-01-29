5ce57a4784e1086cb5d00d83a10dd86ff3d6a288
Maris <maris@born.lv>
2019-01-29 16:54:26 +0200

    .

--- app/Plugins/Attributes/views/modal.blade.php
+++ app/Plugins/Attributes/views/modal.blade.php
@@ -1,8 +1,8 @@
-@section("modal_title")
+@section("modal_title_{$modalId['attribute']}")
     Attribute Value
 @endsection
 
-@section("modal_body")
+@section("modal_body_{$modalId['attribute']}")
     @foreach(languages() as $language)
         <div class="row">
             <div class="col-md-4">Attribute Name ({{$language->name}}):</div>
@@ -16,6 +16,6 @@
     @endforeach
 @endsection
 
-@section("modal_footer")
+@section("modal_footer_{$modalId['attribute']}")
     <button type="button" class="btn btn-success btn-xs saveAttributeValue">Save Attribute Value</button>
 @endsection
