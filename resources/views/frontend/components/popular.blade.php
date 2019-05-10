c001251473c4d5aaf76bf1cc0c411db21929a624
Maris <maris@born.lv>
2019-05-10 17:08:21 +0300

    fix main page

--- resources/views/frontend/components/popular.blade.php
+++ resources/views/frontend/components/popular.blade.php
@@ -1,7 +1,7 @@
 
 <div class="sv-product-card-slider">
     <div class="owl-carousel">
-        @foreach(\App\Plugins\Products\Model\Product::where(['is_highlighted' => 1])->pluck('id')->toArray() as $itemId)
+        @foreach(\App\Plugins\Products\Model\Product::where(['is_highlighted' => 1])->pluck('storage_amount','id')->toArray() as $itemId => $itemAmount)
             @php
                 $item = $cache->getProduct($itemId);
             @endphp
