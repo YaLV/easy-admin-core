07929277ed053361bd8abc524a53e9c8fdc2d6bb
Maris <maris@born.lv>
2019-06-17 16:38:06 +0300

    .

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
-   // Show Legal Forms
-   $('#is_legal').change(function(e){
-      if($(this).is(":checked")) {
-          $('#legalform').removeClass('hidden');
-          return true;
-      }
-      $('#legalform').addClass('hidden');
-   });
+    // Show Legal Forms
+    $('#is_legal').change(function (e) {
+        if ($(this).is(":checked")) {
+            $('#legalform').removeClass('hidden');
+            return true;
+        }
+        $('#legalform').addClass('hidden');
+    });
 
-    jQuery(document).keyup(function(e) {
-        if(e.keyCode===27 && jQuery('body').hasClass('sv-lightbox-open')) {
+    jQuery(document).keyup(function (e) {
+        if (e.keyCode === 27 && jQuery('body').hasClass('sv-lightbox-open')) {
             jQuery('div.sv-lightbox-open .sv-close').click();
         }
     });
 
-    jQuery('.reportClose').click(function(e) {
+    jQuery('.reportClose').click(function (e) {
         e.preventDefault();
         jQuery.post($(this).data('url'));
         el = $(this).parents('.sv-message');
-        if(el) {
+        if (el) {
             el.hide('blind', {}, 500, function () {
                 el.remove();
             });
@@ -26,14 +26,20 @@ $(document).ready(function () {
         return false;
     })
     addToCart();
+
+    $('.sv-close').click(function (e) {
+        e.preventDefault();
+        $(this).parents('.sv-message').remove();
+        return false;
+    });
 });
 
 
 function addToCart() {
     $('.sv-product-card form, .sv-product-title form').unbind().submit(function (e) {
         e.preventDefault();
-        $.post($(this).attr('action'), $(this).serialize(), function(response) {
-            if(response.status) {
+        $.post($(this).attr('action'), $(this).serialize(), function (response) {
+            if (response.status) {
                 showCartAlert();
                 showCartTotals(response);
                 return false;
@@ -46,6 +52,6 @@ function addToCart() {
 }
 
 function showCartTotals(response) {
-    $('.minicart-totals').text(response.contents.cartTotals.toPay+" €");
+    $('.minicart-totals').text(response.contents.cartTotals.toPay + " €");
     $('.cart').addClass('has-items');
 }
