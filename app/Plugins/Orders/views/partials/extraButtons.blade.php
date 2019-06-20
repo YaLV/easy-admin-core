4cd02dd3c7386f91077a3cb0741b1c8dfc4b34f7
Maris <maris@born.lv>
2019-06-20 13:28:24 +0300

    button Names

--- app/Plugins/Orders/views/partials/extraButtons.blade.php
+++ app/Plugins/Orders/views/partials/extraButtons.blade.php
@@ -1,5 +1,8 @@
 <div class="btn-group">
-    <label for="importOrderUpload" style="margin:0px;" class="btn btn-warning btn-xs importOrders"><i class="fas fa-upload"></i></label>
+    <label for="importOrderUpload" style="margin:0px;" class="btn btn-warning btn-xs importOrders">
+        Import Order Update
+        {{--<i class="fas fa-upload"></i>--}}
+    </label>
     <form method="post" style="display:none;">
         <input type="file" name="importFile" accept=".zip,.rar" id="importOrderUpload">
         <input type="hidden" name="controller" value="OrderController" />
@@ -7,7 +10,10 @@
         <input type="hidden" name="method" value="importOrders" />
     </form>
 
-    <label for="importOrderSend" style="margin:0px;" class="btn btn-warning btn-xs importOrderSend"><i class="fas fa-file-pdf"></i></label>
+    <label for="importOrderSend" style="margin:0px;" class="btn btn-warning btn-xs importOrderSend">
+        Import For PDF
+        {{--<i class="fas fa-file-pdf"></i>--}}
+    </label>
     <form method="post" style="display:none;">
         <input type="file" name="importFile" accept=".zip,.rar" id="importOrderSend">
         <input type="hidden" name="controller" value="OrderController" />
@@ -15,8 +21,17 @@
         <input type="hidden" name="method" value="sendEmails" />
     </form>
 
-    <a href="{{ route('orders.sendEmails') }}" class="btn btn-success btn-xs isAjax post ask" data-question="{{ __('translations.SureToSend') }}"><i class="fas fa-envelope"></i></a>
-    <a href="{{ route('orders.exportOrders') }}" class="btn btn-warning btn-xs isAjax post" title="Export"><i class="fas fa-download"></i></a>
-    <a href="{{ route('orders.summary') }}" class="btn btn-info btn-xs isAjax post" title="Order Summary"><i class="fas fa-book"></i></a>
+    <a href="{{ route('orders.sendEmails') }}" class="btn btn-success btn-xs isAjax post ask" data-question="{{ __('translations.SureToSend') }}">
+        Send To Farmers
+        {{--<i class="fas fa-envelope"></i>--}}
+    </a>
+    <a href="{{ route('orders.exportOrders') }}" class="btn btn-warning btn-xs isAjax post" title="Export">
+        Export Orders
+        {{--<i class="fas fa-download"></i>--}}
+    </a>
+    <a href="{{ route('orders.summary') }}" class="btn btn-info btn-xs isAjax post" title="Order Summary">
+        Order Summary
+        {{--<i class="fas fa-book"></i>--}}
+    </a>
 
 </div>
